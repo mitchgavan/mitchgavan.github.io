f53abb9895d05ae6ab906893cdd6ac4bcbce9755
mitchgavan <mitchgavan@Mitchs-MacBook-Pro.local>
2015-07-02 22:21:26 +1000

    js optimisation

--- scripts/header.js
+++ scripts/header.js
@@ -4,22 +4,36 @@ var lastScrollTop = 0;
 var delta = 5;
 var $body = $('body');
 var $navbar = $('#nav');
-var $banner = $('#banner');
 var navbarHeight = $navbar.outerHeight();
-var bannerHeight = $banner.outerHeight();
-var test;
 
 
 $(window).scroll(function(event){
     didScroll = true;
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
+
+if ($body.hasClass('default')) {
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+} else {
+
+    var $banner = $('#banner');
+    var bannerHeight = $banner.outerHeight();
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolledDefault();
+            didScroll = false;
+        }
+    }, 250);
+
+}
 
 function hasScrolled() {
     var st = $(this).scrollTop();
@@ -37,13 +51,34 @@ function hasScrolled() {
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
+            $navbar.removeClass('is-navUp');
+        } 
+    }
+    
+    lastScrollTop = st;
+}
+
+function hasScrolledDefault() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $navbar.addClass('is-navUp');
+        
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
             $navbar.removeClass('is-navUp headerSubpage-transparent');
 
-            if (st < bannerHeight && !$body.hasClass('default')) {
+            if (st < bannerHeight) {
                 $navbar.addClass('headerSubpage-transparent');
             }
-
-            
         } 
     }
     
