1b4a331c1a2b8ec97f2f7f782716c6d926a4087d
Mitch Gavan <mitchgavan@gmail.com>
2026-03-27 11:46:43 +1100

    add dark mode

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 var $ = require('jquery');
 var navToggle = require('./nav');
 var smoothScroll = require('./smoothScroll');
+var initTheme = require('./theme');
 
 import '../scss/style.scss';
 
 $(document).ready(function () {
+  initTheme();
   navToggle();
   smoothScroll();
 });
